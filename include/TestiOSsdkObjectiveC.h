6fa69bd2332343c03e60fa73f634b818187c7a22
cr8142 <cr8142@zebra.com>
2024-03-26 09:56:20 +0530

    second commit

---
+++ include/TestiOSsdkObjectiveC.h
@@ -0,0 +1,15 @@
+//
+//  TestiOSsdkObjectiveC.h
+//  TestiOSsdkObjectiveC
+//
+//  Created by Dhanushka, Adrian on 2024-03-11.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestiOSsdkObjectiveC : NSObject
+
+-(void)methodTest;
+-(NSString*)methodTestOnePrameter:(NSString*)name;
+-(NSString*)methodTestDoublePointer:(NSMutableArray**)nameList;
+@end
